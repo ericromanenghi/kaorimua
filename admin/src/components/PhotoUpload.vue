a9e7b714df2b1f8036c185ba195f60a2fea6cf34
Eric F. Romanenghi <eric.romanenghi@booking.com>
2019-10-17 18:39:12 +0200

    Move upload photo form to a modal

--- admin/src/components/PhotoUpload.vue
+++ admin/src/components/PhotoUpload.vue
@@ -1,31 +1,33 @@
 <template>
     <div id="photo-upload-container">
-        <button @click="onCreate()" class="waves-effect waves-light btn">
+        <a @click="onCreate()" class="waves-effect waves-light btn modal-trigger" :href="'#modal-photo-upload'+gallery_id">
             Upload photo
-        </button>
-        <div v-if="display_form">
-            <div v-if="!submited" class="upload-photo-form">
-                <h3>Upload photo</h3>
-                <form class="" @submit.prevent="onSubmit">
-                    <p>
-                        <label for="name">Photo name:</label>
-                        <input :id="'photo-name-'+gallery_id" v-model="photo_name" placeholder="name">
+        </a>
+        <div :id="'modal-photo-upload'+gallery_id" class="modal">
+            <div class="modal-content">
+                <div v-if="!submited" class="upload-photo-form">
+                    <h3>Upload photo</h3>
+                    <form class="" @submit.prevent="onSubmit">
+                        <p>
+                            <label for="name">Photo name:</label>
+                            <input :id="'photo-name-'+gallery_id" v-model="photo_name" placeholder="name">
+                        </p>
+                        <p>
+                            <button class="btn waves-effect waves-light" type="submit" name="action">
+                                Submit
+                                <i class="material-icons right">send</i>
+                            </button>
+                        </p>
+                    </form>
+                </div>
+                <div v-else class="photo-upload-msg">
+                    <p v-if="success" class="green-text">
+                        <i class="material-icons left">check</i>Photo successfully added to gallery!
                     </p>
-                    <p>
-                        <button class="btn waves-effect waves-light" type="submit" name="action">
-                            Submit
-                            <i class="material-icons right">send</i>
-                        </button>  
+                    <p v-else class="red-text">
+                        <i class="material-icons left">error</i>Something went wrong, please try again =S
                     </p>
-                </form>
-            </div>
-            <div v-else class="photo-upload-msg">
-                <p v-if="success" class="green-text">
-                    <i class="material-icons left">check</i>Photo successfully added to gallery!
-                </p>
-                <p v-else class="red-text">
-                    <i class="material-icons left">error</i>Something went wrong, please try again =S
-                </p>
+                </div>
             </div>
         </div>
     </div>
@@ -45,6 +47,9 @@ export default {
         };
     },
     mounted() {
+        let elems = this.$el.querySelectorAll('.modal');
+        // eslint-disable-next-line
+        let instances = M.Modal.init(elems);
     },
     methods: {
         onCreate() {
