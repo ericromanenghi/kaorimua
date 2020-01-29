c3c6797df686095875dae8c1cc9a2f5237d5a8b1
Lukasz Rados <lukaszrados@gmail.com>
2020-01-29 22:25:45 +0000

    Add photos animation on the homepage.

--- frontend/src/components/pages/Home.vue
+++ frontend/src/components/pages/Home.vue
@@ -1,12 +1,40 @@
 <template>
     <div class='container'>
-        <div class='cover'></div>
+        <div class='cover'>
+            <div class='cover__photo' v-for='(photo, index) in photos' :key='photo' :style='{ backgroundImage: `url(${photo})`, opacity: currentPhotoIndex === index ? 1 : 0 }'></div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data () {
+        return {
+            photos: [
+                `${process.env.BASE_URL}photos/home/photo-1.jpg`,
+                `${process.env.BASE_URL}photos/home/photo-2.jpg`,
+                `${process.env.BASE_URL}photos/home/photo-3.jpg`,
+            ],
+            currentPhotoIndex: 0,
+            interval: null,
+        }
+    },
+
+    methods: {
+        changeImage () {
+            ++this.currentPhotoIndex
+            if (this.currentPhotoIndex >= this.photos.length) this.currentPhotoIndex = 0
+        }
+    },
+
+    mounted () {
+        this.interval = setInterval(this.changeImage, 5000)
+    },
+
+    destroyed () {
+        clearInterval(this.interval)
+        this.interval = null
+    }
 }
 </script>
 
@@ -14,15 +42,23 @@ export default {
 @import './../../scss/variables.scss';
 
 .cover {
-    background-image: url(/photos/home/photo.jpg);
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-size: cover;
     display: block;
     height: calc(100vh - 164px);
     margin: 10px;
     position: relative;
     width: calc(100% - 20px);
+}
+.cover__photo {
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    transition: opacity .6s linear;
+    width: 100%;
+    z-index: 3;
 }
 .cover:after {
     background: rgba($color-secondary, .5);
@@ -33,6 +69,7 @@ export default {
     position: absolute;
     top: 0;
     width: 100%;
+    z-index: 4;
 }
 
 @media screen and (min-width: 700px) {
