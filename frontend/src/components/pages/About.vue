b20a0d256c3b5a1577fe1d4036ab5eb5bad0d283
Eric F. Romanenghi <ericromanenghi@gmail.com>
2020-02-22 19:56:29 +0100

    Change about page text

--- frontend/src/components/pages/About.vue
+++ frontend/src/components/pages/About.vue
@@ -1,9 +1,30 @@
 <template>
     <TextPage title='About me'>
         <p class='textpage__intro'>Love yourself as you are<br>and enjoy doing what you do</p>
-        <p>My name is Kaori, a passionate beauty lover &amp; freelance makeup artist, hairstylist &amp; winner of the Talent Award of 2018 in The Netherlands. After finishing my Makeup Artist course at the Glamour Makeup I started freelancing straight away. </p>
-        <p>During my career as a makeup artist &amp; hairstylist, I’ve been specializing in editorial work within the fashion and beauty industry. Along with that, I started to do commercials, advertising, corporate films, and private events.</p>
-        <p>Over the years I also managed to obtain various publications in magazines and I also work for different brands within the Netherlands.</p>
+
+        <p>Hello and Welcome!</p>
+
+        <p>
+            My name is Kaori and I am a Professional Makeup artist. I am originally from Buenos Aires city (Argentina) but I have been living in Amsterdam (The Netherlands) for the last couple of years.
+        </p>
+
+        <p>
+            Since I was very young, I loved “playing” with makeup and I always identified myself as an artistic and creative person. That’s why, on 2018 I “took the leap of faith” and decided to take a professional makeup Course, and I loved every single minute of it!. 
+        </p>
+
+        <p>
+            From that moment on, I couldn’t stop!, to gain more experience and to start creating content I started organizing photoshoots, I started to do some network with other (amazing!) professionals and also started accepting challenges and opportunities that I got from online researches. 
+        </p>
+
+        <p>
+            I have also invested on getting even more professional trainings in different cities such as London (UK), Amsterdam (NL), Paris (Fr), Milano (It) and in Buenos Aires (Arg).
+        </p>
+
+        <p>
+            I hope you enjoy the content on my website! Please, feel free to follow me on my social media accounts and to contact me for any inquiries or information I might help you with.
+        </p>
+
+        <p>XO</p>
     </TextPage>
 </template>
 
