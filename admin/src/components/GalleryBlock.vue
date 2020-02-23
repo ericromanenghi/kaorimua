f26a7a28aeb23ea5329b3eeb4529f8234ec3eef4
Eric F. Romanenghi <ericromanenghi@gmail.com>
2020-02-23 16:25:41 +0100

    Align gallery block elements properly

--- admin/src/components/GalleryBlock.vue
+++ admin/src/components/GalleryBlock.vue
@@ -1,19 +1,29 @@
 <template>
     <div>
-        <h4 @click="toggleActive">{{ name }}</h4>
-        <div v-if="active"> 
-            <p>
-                <photo-upload :gallery_id="id"></photo-upload>
-            </p>
-            <p>
-                <gallery-edit :gallery_id="id" :name="name" :photographer="photographer" :model="model"></gallery-edit>
-            </p>
-            <p>
-                Photographer: {{ photographer }}
-            </p>
-            <p>
-                Model: {{ model }}
-            </p>
+        <div class="row">
+            <div class="col s12">
+                <h4 @click="toggleActive">{{ name }}</h4>
+            </div>
+        </div>
+        <div v-if="active">
+            <div class="row">
+                <div class="col s3">
+                    <photo-upload :gallery_id="id"></photo-upload>
+                </div>
+                <div class="col s3">
+                    <gallery-edit :gallery_id="id" :name="name" :photographer="photographer" :model="model"></gallery-edit>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12">
+                    Photographer: {{ photographer }}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12">
+                    Model: {{ model }}
+                </div>
+            </div>
             <gallery-photos :gallery_id="id"></gallery-photos>
         </div>
     </div>
