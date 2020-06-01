cf1bf7b60ff09cae1660e7fa4c2191a7aacfb229
Eric F. Romanenghi <ericromanenghi@gmail.com>
2020-06-01 22:29:10 +0200

    Resize images after uploading

--- backend/lib/file_utils.py
+++ backend/lib/file_utils.py
@@ -2,6 +2,9 @@ import uuid
 import os
 
 from flask import current_app
+from PIL import Image
+
+MAX_HEIGHT = 1200
 
 def allowed_extensions():
     return current_app.config['ALLOWED_EXTENSIONS']
@@ -21,7 +24,21 @@ def secure_filename(filename):
     return '.'.join([str(uuid.uuid4()), extension])
 
 def save_file(file, filename):
-    file.save(get_full_path(filename))
+    full_path = get_full_path(filename)
+    file.save(full_path)
+    try:
+        resize_image(full_path)
+    except:
+        current_app.logger.error("Image couldn't be resized")
 
 def get_full_path(filename):
     return os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+
+def resize_image(filename):
+    img = Image.open(filename)
+    (width, height) = img.size
+    if height > MAX_HEIGHT:
+        ratio = width / height
+        new_size = (round(MAX_HEIGHT * ratio), MAX_HEIGHT)
+        new_img = img.resize(new_size, Image.LANCZOS)
+        new_img.save(filename)
